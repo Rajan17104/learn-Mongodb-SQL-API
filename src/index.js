437081ad4d44742914d42805538650e66cc4fdd9
Akshay-Padaliya <patel.akshay43@gmail.com>
2024-06-04 22:45:05 +0530

    Connect to Admin

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 
 const express  = require("express");
-
 const app = express();
-
 const route = require("./routes/api/v1/index");
 const connectDb = require("./db/mogoosdb");
+var cors = require("cors");
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/v1", route);
 
-
-
 connectDb();
 
-app.listen(3000,()=>{
-    console.log("serever created 3000");
+app.listen(6000,()=>{
+    console.log("serever created 6000");
 })
