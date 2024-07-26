21f0156ea98d98c94d3debfd12b2edf5a370b190
princychodvadiya <princychodvadiya721@gmail.com>
2024-07-26 08:56:41 +0530

    logout , google login

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ const app = express();
 const route = require("./routes/api/v1/index");
 const connectDb = require("./db/mogoosdb");
 var cors = require("cors");
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
+const googleLoginProvider = require("./utils/provider");
 
 app.use(cors())
 app.use(cookieParser())
 app.use(express.json());
+app.use(require('express-session')({ secret: 'QWERTYUIOP', resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/api/v1", route);
 
 connectDb();
+
+googleLoginProvider()
 
 app.listen(9000,()=>{
     console.log("serever created 9000");
---
+++ src/utils/provider.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const googleLoginProvider = async() =>{
+try {
+    passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://www.example.com/auth/google/callback"
+      },
+      function(accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+          return cb(err, user);
+        });
+      }
+    ));
+} catch (error) {
+    
+}
+}
+
+module.exports = googleLoginProvider;
